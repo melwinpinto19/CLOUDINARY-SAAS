2f8e092a7cae67dd1209352e55638cd9983e1355
melwinpinto19 <melwinpinto531@gmail.com>
2025-04-04 07:31:38 +0530

    fix: made the cloudinary api secret only accesible to server side

--- src/app/api/v1/upload/image/route.ts
+++ src/app/api/v1/upload/image/route.ts
@@ -7,7 +7,7 @@ import { imageUploadResponse } from "@/types";
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function POST(request: NextRequest) {
--- src/app/api/v1/upload/video/route.ts
+++ src/app/api/v1/upload/video/route.ts
@@ -10,7 +10,7 @@ const prisma = new PrismaClient();
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function POST(request: NextRequest) {
